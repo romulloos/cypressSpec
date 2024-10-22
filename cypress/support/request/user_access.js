8fdf2065a9eee845019d1d3737672b5e0e6c0023
Romullo <romullo.os@gmail.com>
2024-10-22 20:31:33 -0300

    inclusao de request de criar usuario

--- cypress/support/request/user_access.js
+++ cypress/support/request/user_access.js
@@ -1,33 +1,59 @@
 class UserAccess {
 
-    loginUser(email, loginPassword, loginUser){
+    accountCreateRequest() {
+        cy.request({
+            method: 'POST',
+            url: '/accountservice/accountrest/api/v1/register',
+            headers: {
+                accept: '*/*',
+                'Content-Type': 'application/json',
+            },
+            body: {
+                accountType: 'USER',
+                address: `${address}`,
+                allowOffersPromotion: true,
+                aobUser: true,
+                cityName: `${cityName}`,
+                country: `${country}`,
+                email: `${email}`,
+                firstName: `${firstName}`,
+                lastName: `${lastName}`,
+                loginName: `${loginName}`,
+                password: `${password}`,
+                phoneNumber: `${phoneNumber}`,
+                stateProvince: `${stateProvince}`,
+                zipcode: `${zipcode}`,
+            },
+        }).as('accountCreateRequest');
+    }
+    loginUser(email, loginPassword, loginUser) {
         cy.request({
             method: 'POST',
             url: '/accountservice/accountrest/api/v1/login',
             headers: {
-                accept: "*/*",
-                "Content-Type": "application/json",
+                accept: '*/*',
+                'Content-Type': 'application/json',
             },
             body: {
                 email: `${email}`,
                 loginPassword: `${loginPassword}`,
                 loginUser: `${loginUser}`,
             }
-        }).as("loginUser");
+        }).as('loginUser');
     }
-    logoutUser(accountId, token){
+    logoutUser(accountId, token) {
         cy.request({
             method: 'POST',
             url: '/accountservice/accountrest/api/v1/logout',
             headers: {
-                accept: "*/*", 
-                "Content-Type": "application/json"
+                accept: '*/*',
+                'Content-Type': 'application/json'
             },
             body: {
                 accountId: `${accountId}`,
                 token: `${token}`
-                },
-        }).as("logoutUser");
+            },
+        }).as('logoutUser');
     }
 }
 export default UserAccess();
